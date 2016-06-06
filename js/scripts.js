f3d6b55e93a2dc906b983f02b1b5f275da248b31
Dave Strus <dave@getfretless.com>
2016-06-06 15:54:05 -0400

    Add mutants and pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,24 @@ $('a[data-remote="true"]').on('click', function(e) {
   });
 });
 
+$('a[data-remote-mutants="true"]').on('click', function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr('href'),
+    method: 'get',
+    success: loadMutants
+  });
+});
+
+$('a[data-remote-pokemon="true"]').on('click', function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr('href'),
+    method: 'get',
+    success: getAllPokemon
+  });
+});
+
 function loadResults(data) {
   if (data.firstName) {
     people.push(data);
@@ -23,6 +41,38 @@ function loadResults(data) {
   listPeople();
 }
 
+function loadMutants(data) {
+  $.each(data, function(i, mutant) {
+    people.push({
+      firstName: mutant.mutant_name,
+      lastName: '[' + mutant.real_name + ']',
+      secret: mutant.power
+    });
+  });
+  listPeople();
+}
+
+function getAllPokemon(data) {
+  $.each(data.results, function(i, pokemon) {
+    $.ajax({
+      url: pokemon.url,
+      method: 'get',
+      success: loadPokemon
+    });
+  });
+  setTimeout(function() {
+    listPeople();
+  }, 5000);
+}
+
+function loadPokemon(pokemon) {
+  people.push({
+    firstName: pokemon.name,
+    lastName: '',
+    secret: pokemon.abilities[0].ability.name
+  });
+}
+
 function listPeople() {
   $('#people').slideUp();
   $('#people').empty();
@@ -30,11 +80,14 @@ function listPeople() {
     var item = $('#template').clone().attr('id', '');
     item.html(item.html().replace('{{ person.firstName }}', person.firstName)
       .replace('{{ person.lastName }}', person.lastName)
-      .replace('{{ person.secret }}', person.secret));
-    item.removeClass('hide');
+      .replace('{{ person.secret }}', person.secret))
+      .removeClass('hide');
     $('#people').append(item);
-    $('#people').slideDown();
   });
+
+  $('#people').slideDown();
 }
+
+
 
 listPeople();
